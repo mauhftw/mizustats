9b63b854b5d2f4d952c67d733acf70e5c9564259
Mauricio 'rambe' Ashimine <maashimine@gmail.com>
2016-01-17 20:41:47 -0300

    Models & Relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function role() {
+        return $this->hasOne('App\Models\Role')
+    }
 }
