cdc43987d985a065a31b39539afc588266865d1e
Mauricio 'rambe' Ashimine <maashimine@gmail.com>
2016-03-02 18:02:18 -0300

    avances graficos

--- app/Http/Controllers/AdminReportsController.php
+++ app/Http/Controllers/AdminReportsController.php
@@ -9,36 +9,87 @@ use App\Http\Controllers\Controller;
 use App\Models\WaterRegister;
 use App\Models\User;
 use App\Models\Role;
+use Datatables;
 use Gate;
 use DB;
+use stdClass;
 
 class AdminReportsController extends Controller {
 
-  public function __construct()
-  {
-      $role = Role::where('name','admin')->first();
+  public function __construct() {
+
+      $role = Role::where('name','admin')->first();   //Mejor guardar el role en la session
       $this->middleware('auth');
       $this->authorize('auth',$role);
 
-  }
+}
+
 
   public static function index() {
-
-    $role = Role::where('name','admin')->first();
-
-    /*if (Gate::denies('auth', $role)) {
-        abort(403,'Nope');
-      }*/
 
       return view('dashboard.index');
   }
 
-  public static function showRegisteredUsers() {
+  public static function getDatatable() {
+
+      /*number of users*/
       $users = User::select('id')->active()->count();
-      echo $users;
+
+      /*consumption*/
+      $today = date("Y-m-d");
+      $consumption = WaterRegister::where('date', '=', $today)
+                        ->where('state', '=', 'Mendoza')  //ver si conviene poner id o el state
+                        ->sum('value');
+
+      /*consumption per hour*/
+      $consumptionPerHour = $consumption/60;
+
+      /*largest consumer*/
+      $consumers = DB::table('water_registers')->select('city',DB::raw('sum(value) as total'))
+                          ->where('date', '=', $today)
+                          ->where('state','=','Mendoza')
+                          ->orderBy('total','desc') //ordeno de mayor a menor
+                          ->groupBy('city')
+                          ->get();
+      //manejar errores
+     $largestConsumer = reset($consumers);  //1er elemento, mayor
+     $smallestConsumer = end($consumers);  //ultimo elemento, menor
+
+     /*Total month consumption*/
+     $first_day = date("Y-m-01");
+     $last_day = date("Y-m-t");
+
+     $totalConsumption = WaterRegister::where('state','=','Mendoza')
+                           ->whereBetween('date',[$first_day,$last_day])
+                           ->sum('value');
+
+    /*$aux = [
+       'users' => $users,
+       'consumption' => $consumption,
+       'consumptionPerHour' => $consumptionPerHour,
+       'largestConsumer' => $largestConsumer->city,
+       'largestConsumerValue' => $largestConsumer->total,
+       'totalConsumption' => $totalConsumption,
+     ];*/
+
+     $aux = new StdClass;
+     $aux->users = $users;
+     $aux->consumption = $consumption;
+     $aux->consumptionPerHour = $consumptionPerHour;
+     $aux->largestConsumer = $largestConsumer->city;
+     $aux->largestConsumerValue = $largestConsumer->total;
+     $aux->totalConsumption = $totalConsumption;
+
+     $data = [collect($aux)];   //hack super villa, para transformar a coleccion de datos
+
+
+    $datatable = Datatables::of(collect($data));
+    return $datatable->make(true);
+
+
   }
 
-  public static function showWaterConsumption() {
+/*  public static function showWaterConsumption() {
       $today = date("Y-m-d");   //variable protegida?
       $consumption = WaterRegister::where('date', '=', $today)
                         ->where('state', '=', 'Mendoza')  //ver si conviene poner id o el state
@@ -81,7 +132,7 @@ class AdminReportsController extends Controller {
                               ->sum('value');
         echo $total;
 
-  }
+  }*/
 
   public static function showCitiesMonthsConsumptionGraph() { //todas las ciudades en el mes
         $today = date("Y-m-d");
@@ -94,7 +145,14 @@ class AdminReportsController extends Controller {
                         ->orderBy('total','desc') //ordeno de mayor a menor
                         ->groupBy('city')
                         ->get();
-        dd($consumers);
+        //dd($consumers);
+        $test = [
+          ['ciudad' => 220],
+          ['las heras' => 15],
+          ['pepe' => 22],
+        ];
+        //return json_encode($test);
+        return response()->json($consumers);
   }
 
   public static function showCitiesDaysConsumptionGraph() {   //todos las ciudades en el dia
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,4 +3,95 @@
 @section('page-description','Panel de reportes')
 @section('content')
 
-@endsection
+<div class="col-md-12">
+    <section class="panel">
+        <header class="panel-heading">
+            <div class="panel-actions">
+                <a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+            </div>
+        </header>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-bordered table-striped" id="datatable" data-url="{{{ url('dashboard/data') }}}">
+                        <thead>
+                            <tr>
+                                <th class="col-md-2 center">Usuarios activos</th>
+                                <th class="col-md-2 center">Consumo acumulado del dia [lts]</th>
+                                <th class="col-md-2 center">Consumo/hora [lts]</th>
+                                <th class="col-md-2 center">Mayor consumidor</th>
+                                <th class="col-md-2 center">Consumo del mayor consumidor</th>
+                                <th class="col-md-3 center">Consumo acumulado del mes</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                  <div class="col-md-12">
+                    <div id="chart_div"></div>
+                  </div>
+            </div>
+        </div>
+    </section>
+
+@stop
+@section('scripts')
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#datatable').dataTable({
+        processing: true,
+        serverSide: true,
+        bFilter: false,
+        paging: false,
+        ordering: false,
+        info: false,
+        //language: datatable_spanish,
+        ajax: $('#datatable').data('url'),
+        columns: [
+        {data: 'users', name: 'users', className: "center"},
+        {data: 'consumption', name: 'consumption'},
+        {data: 'consumptionPerHour', name: 'consumptionPerHour'},
+        {data: 'largestConsumer', name: 'largestConsumer'},
+        {data: 'largestConsumerValue', name: 'largestConsumerValue', className: "center"},
+        {data: 'totalConsumption',name: 'totalConsumption', className: "center"}
+        ]
+    });
+});
+</script>
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+
+// Load the Visualization API and the piechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var jsonData = $.ajax({
+      url: "dashboard/cities/month",
+      dataType: "json",
+      async: false
+      }).responseText;
+
+  // Create our data table out of JSON data loaded from server.
+  var data = new google.visualization.DataTable(jsonData);
+  data.addColumn('string', 'cities');
+  data.addColumn('number', 'water');
+  console.log(jsonData);
+  var options = {
+    title: 'Consumo del mes',
+    hAxis: {title: 'Consumo', titleTextStyle: {color: 'green'}},
+    vAxis: {title: 'Ciudad', titleTextStyle: {color: '#FF0000'}},
+  //  backgroundColor:'#ffffcc',
+  //  legend:{position: 'bottom', textStyle: {color: 'blue', fontSize: 13}},
+    width:320,
+    height:240
+};
+  // Instantiate and draw our chart, passing in some options.
+
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+</script>
+@stop
